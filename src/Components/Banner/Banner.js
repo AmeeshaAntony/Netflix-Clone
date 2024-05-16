b7d98f656e147ecab795a868b511cdb82f2d3d25
AmeeshaAntony <ameesha468@gmail.com>
2024-05-16 19:48:13 +0530

    Second Commit

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import "./Banner.css";
+import React, { useState, useEffect } from 'react';
+import axios from '../../axios';
+import './Banner.css';
+
+// Create an instance of axios with base URL
+
 function Banner() {
+  const [movie, setMovie] = useState();
+
+  useEffect(()=>{
+
+  },[])
+
   return (
     <div className='banner'>
       <div className='content'>
-        <h1 className='title'>Movie</h1>
-        <div className='banner-button'>
-            <button className='button'>Play</button>
-            <button className='button'>My List</button>
-        </div>
+        {movie && (
+          <>
+            <h1 className='title'>Movie : {movie.title}</h1>
+            <div className='banner-button'>
+              <button className='button'>Play</button>
+              <button className='button'>My List</button>
+            </div>
+          </>
+        )}
         <h1 className='desc'>qwertyuioplkjhgfdsaxzcvbnm mnjnjdfnjnejfergjer. jnjejekekkenkkrmbkrm</h1>
       </div>
       <div className='fade'></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '883343d39ffa8d8d2817192f7c49c86b';
